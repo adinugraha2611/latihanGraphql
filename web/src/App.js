da94dd358a115edf5609ce63c844cbe8dd091b8f
adinugraha2611 <m.adinugraha.ug@gmail.com>
2020-10-07 11:14:55 +0700

    Reorganize project directories: - divide projecct into 2 different folder; /api and /web - add /util/seed to /api (useful to generate fake local users and notes database) - updated /web: chapter 13 done

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from '/pages';
+import GlobalStyle from './components/GlobalStyle';
+
+const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      <Pages />
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ web/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../components/Button';
+
+const Home = () => {
+  return (
+    <div>
+      <p>This is the home page</p>
+      <Button>click me!</Button>
+    </div>
+  );
+};
+export default Home;
