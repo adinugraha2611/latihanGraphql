90f2c63aea7ad2235a9b14c77cfb5172bcc021c3
adinugraha2611 <m.adinugraha.ug@gmail.com>
2020-10-12 19:19:44 +0700

    shows note feeds in home page

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+// package to render markdown on the page
+import ReactMarkdown from 'react-markdown';
 import Button from '../components/Button';
 
+// our GraphQL query, stored as a variable
+const GET_NOTES = gql`
+  query NoteFeed($cursor: String) {
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        createdAt
+        content
+        favoriteCount
+        author {
+          username
+          id
+          avatar
+        }
+      }
+    }
+  }
+`;
+
 const Home = () => {
+  // query hook
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+  // if the data is loading, display a loading message
+  if (loading) return <p>Loading...</p>;
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error!</p>;
+  // if the data is successful, display the data in our UI
   return (
     <div>
-      <p>This is the home page</p>
-      <Button>click me!</Button>
+      {data.noteFeed.notes.map(note => (
+        <article key={note.id}>
+          <img
+            src={note.author.avatar}
+            alt={`${note.author.username} avatar`}
+            height="50px"
+          />{' '}
+          {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
+          <ReactMarkdown source={note.content} />
+        </article>
+      ))}
     </div>
   );
 };
+
 export default Home;
